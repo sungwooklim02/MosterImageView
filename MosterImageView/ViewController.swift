91fcc1bc0ab676b4095f2674e8d9d017905dcf9b
kwackjihoon <chunjoo4@naver.com>
2019-03-28 15:34:55 +0900

    pro complete

--- MosterImageView/ViewController.swift
+++ MosterImageView/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var monsterImage: UIImageView!
+    
+    @IBOutlet weak var lblCount: UILabel!
+    
+    var count = 1;
+    var sw = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btnChange(_ sender: Any) {
+        
+        monsterImage.image = UIImage(named: "frame\(count).png")
+        //lblCount.text = String(count);
+        lblCount.text = "frame\(count).png"
+        
+        if (sw == true){
+            count += 1;
+            if(count == 6){
+                sw = false;
+            }
+        }
+        if (sw == false){
+            count -= 1;
+            if(count == 1){
+                sw = true;
+            }
+        }
+        
     }
-
+    
 
 }
 
